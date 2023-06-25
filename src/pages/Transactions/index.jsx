d8dc5ddde047724522db003d1b617f466012f015
lostmart <lostmart@hotmail.com>
2023-06-25 12:38:51 +0200

    n

--- src/pages/Transactions/index.jsx
+++ src/pages/Transactions/index.jsx
@@ -1,35 +1,32 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { Navigate, useParams } from 'react-router-dom'
 import Section from '../../components/UI/organisms/Section'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 import { fetchUserThunk } from '../../features/userSlice'
-import { fetchTransactionThunk } from '../../features/transactionSlice'
 
-import { FaChevronDown } from 'react-icons/fa'
+ import {transactionsData} from '../../mockup/transactionsData'
 
 import styles from './index.module.css'
 import Title from '../../components/UI/atoms/Title'
 import Table from '../../components/UI/organisms/Table'
 
 const Transactions = () => {
+	
 	const dispatch = useDispatch()
 	const user = useSelector((state) => state.user)
-	const transactions = useSelector((state) => state.transactions)
 	const { id } = useParams()
 
-	// const [selectedAccount, setSelectedAccount] = useState(null)
-
-	// useEffect(() => {
-	// 	dispatch(fetchUserThunk())
-	// 	dispatch(fetchTransactionThunk())
-	// }, [])
-
-	if (transactions.data) {
-		const selectedAccount =
-			transactions.data[1].userTransactions.accounts.filter(
-				(account) => account.transactionId === id
-			)[0]
+	
+	useEffect(() => {
+		dispatch(fetchUserThunk())
+	}, [])
+	
+	console.log(transactionsData)
+	if (user.data) {
+		const selectedAccount = transactionsData.userTransactions.accounts.filter(
+			(account) => account.transactionId === id
+		)[0]
 		return (
 			<main className="main bg-dark">
 				<Section className={styles.transaction}>
@@ -51,7 +48,7 @@ const Transactions = () => {
 			</main>
 		)
 	} else {
-		return <Navigate to="/login" />
+		return <div>algo terrible ha sucedido !!</div>
 	}
 }
 
